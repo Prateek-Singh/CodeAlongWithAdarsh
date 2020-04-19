ce6e9e187b52570fc41de9faffc5282244b7fde6
Prateek-Singh <singhprateek28@gmail.com>
2020-04-19 16:53:43 +0530

    Initial commit for toString demo

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import app.constructor_demo.ConstructorDemoApp;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new ConstructorDemoApp().main();
+    }
+}
---
+++ src/app/constructor_demo/ConstructorDemoApp.java
@@ -0,0 +1,50 @@
+package app.constructor_demo;
+
+public class ConstructorDemoApp {
+    public void main() { 
+        Person person = new Person("Adarsh", "Singh", 21);
+        System.out.println(person);    
+    }
+}
+
+
+class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s %s, Age: %d...!!!", firstName, lastName, age);
+    }                
+}
